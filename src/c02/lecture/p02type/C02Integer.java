6d58d4c8b95d11f860aa0044c2efc831659384c7
Hongbin Yoon <popyto@naver.com>
2024-02-19 15:44:42 +0900

    처음 만들어본 자바 코드

---
+++ src/c02/lecture/p02type/C02Integer.java
@@ -0,0 +1,23 @@
+package c02.lecture.p02type;
+
+public class C02Integer {
+    public static void main(String[] args) {
+        //정수타입
+        //int
+        int a = 5;
+        int b =10;
+        b = 30;
+        b = 30_000_000;
+        b = -30_000_000;
+        b = 0;
+        b = 2_147_483_647;
+
+        //b = 3.14; //x
+
+        //long
+        long c = 2_147_483_648L;
+
+        //byte, short
+
+    }
+}
---
+++ src/c02/sec01/VariableInitializationExample.java
@@ -0,0 +1,20 @@
+package c02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+
+        int value;
+        value = 10;
+        // 값이 없는 변수를 사용할 수 없음
+
+        int result =value +10;
+
+        System.out.println(result);
+    }
+}
+
+
+
+
+
+
