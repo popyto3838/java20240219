2f6a5df6f86eb84befbb903193fe274ccc0feca6
Hongbin Yoon <popyto@naver.com>
2024-02-19 12:41:07 +0900

    처음 만들어본 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,13 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("안녕 세번쨰");
+        System.out.println("또인사하기");
+        System.out.println("또또 인사하기");
+        System.out.println("한번더하기");
+        System.out.println("찐막인사");
+        System.out.println("햄버거");
+        System.out.println("제육");
+        System.out.println("또육");
+
+    }
+}
