ed7a843469c166878eb805cfc6f8f52722ebbbc0
Hongbin Yoon <popyto@naver.com>
2024-03-20 11:08:40 +0900

    afds

---
+++ src/ch15/sec05/exam04/ComparatorExample.java
@@ -0,0 +1,20 @@
+package ch15.sec05.exam04;
+
+import java.util.TreeSet;
+
+public class ComparatorExample {
+    public static void main(String[] args) {
+        // TreeSet<Fruit> treeSet = new TreeSet<>(new FruitComparator());
+        TreeSet<Fruit> treeSet = new TreeSet<>((x, y) -> x.price - y.price);
+
+        treeSet.add(new Fruit("포도", 3000));
+        treeSet.add(new Fruit("수박", 10000));
+        treeSet.add(new Fruit("딸기", 6000));
+
+        for (Fruit fruit : treeSet) {
+            System.out.println(fruit.name + ":" + fruit.price);
+
+        }
+
+    }
+}
--- src/example/Solution9.java
+++ src/example/Solution9.java
@@ -1,9 +1,26 @@
 package example;
+//414
+
+//중복없이 정렬했을때, 3번째로 큰값 구하기, 3번째큰게없으면 가장큰값리턴
+//Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+//input nums =[3,2,1]
+//output : 1
+
+import java.util.TreeSet;
 
 public class Solution9 {
     public int thirdMax(int[] nums) {
 
-        return 0;
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num : nums) {
+            set.add(num);      // 트리셋에넣으면 정렬 자동으로됨,크기순
+        }
+        if (set.size() >= 3) {
+            set.pollLast();
+            set.pollLast();
+        }
+
+        return set.pollLast();
     }
 
 }
